cf5011ff8b9e2f14a2ffdf67a05cba0169115ece
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-08-24 21:17:40 -0300

    Update renderer.js

--- JavaScript/renderer.js
+++ JavaScript/renderer.js
@@ -1,1 +1,15 @@
 var gl;
+
+function loadShader(src)
+{
+	//vertex
+	var vertex=document.createElement('script');
+	vertex.src=src+'vertex.glsl';
+	vertex.type='notjs'
+	document.body.appendChild(vertex);
+	//fragment
+	var fragment=document.createElement('script');
+	fragment.src=src'fragment.glsl';
+	fragment.type='notjs'
+	document.body.appendChild(fragment);
+}
