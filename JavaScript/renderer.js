2b8614972449f9515d73d79ebf7b6629f51c2c54
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-08-25 19:59:04 -0300

    Update renderer.js

--- JavaScript/renderer.js
+++ JavaScript/renderer.js
@@ -3,15 +3,15 @@ var gl;
 function loadShader(src)
 {
 	//vertex
-	var vertex=document.createElement('script');
-	vertex.src=src+'/vertex.glsl';
-	vertex.type='notjs';
-	document.body.appendChild(vertex);
+	//var vertex=document.createElement('script');
+	//vertex.src=src+'/vertex.glsl';
+	//vertex.type='notjs';
+	//document.body.appendChild(vertex);
 	//fragment
-	var fragment=document.createElement('script');
-	fragment.src=src+'/fragment.glsl';
-	fragment.type='notjs';
-	document.body.appendChild(fragment);
-	document.getElementById("test").innerHTML="test fragment 4";
+	//var fragment=document.createElement('script');
+	//fragment.src=src+'/fragment.glsl';
+	//fragment.type='notjs';
+	//document.body.appendChild(fragment);
+	document.getElementById("test").innerHTML="test fragment";
 	return;
 }
