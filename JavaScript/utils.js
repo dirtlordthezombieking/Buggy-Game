8e0c5c51069bcf7818dfac642320750cafeb8be4
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-08-25 20:11:48 -0300

    Update utils.js

--- JavaScript/utils.js
+++ JavaScript/utils.js
@@ -1,13 +1,13 @@
 function loadJavaScript(src)
 {
-	var sript=document.createElement('script');
+	var sript=document.createElement("script");
 	script.src=src;
 	document.body.appendChild(script);
 	return script;
 }
 function loadScript(src,type)
 {
-	var sript=document.createElement('script');
+	var sript=document.createElement("script");
 	script.src=src;
 	script.type=type;
 	document.body.appendChild(script);
