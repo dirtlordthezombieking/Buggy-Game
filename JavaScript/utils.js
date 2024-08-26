877701aa4c948e1c1ebbff2d0597022fed162db6
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-08-26 09:52:28 -0300

    Update utils.js

--- JavaScript/utils.js
+++ JavaScript/utils.js
@@ -8,6 +8,7 @@ function loadJavaScript(src)
 	return script;
 }
 //https://github.com/dirtlordthezombieking/Buggy-Game/blob/dirtlordthezombieking-patch-6/JavaScript%2Frenderer.js
+//https://raw.githubusercontent.com/dirtlordthezombieking/LoreBook/main/Images/sketch-1641141441537.png" alt="LoreBook"></a>
 function loadScript(src,type)
 {
 	let script=document.createElement("script");
