fa955fe551f082e26581ff7febf166ff68a2a9d1
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-08-26 10:53:32 -0300

    Update utils.js

--- JavaScript/utils.js
+++ JavaScript/utils.js
@@ -1,10 +1,16 @@
 function loadJavaScript(src)
 {
+	document.getElementById("test").innerHTML="test script 1";
 	let script=document.createElement("script");
+	document.getElementById("test").innerHTML="test script 2";
 	script.src=src;
+	document.getElementById("test").innerHTML="test script 3";
 	script.type="text/javascript";
+	document.getElementById("test").innerHTML="test script 4";
 	//script.async=true;
+	document.getElementById("test").innerHTML="test script 5";
 	document.body.appendChild(script);
+	document.getElementById("test").innerHTML="test script 6";
 	return script;
 }
 //https://raw.githubusercontent.com/dirtlordthezombieking/BuggyGame/
