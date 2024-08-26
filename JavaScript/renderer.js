d5bed9d63cc0fb8684950accdb9ddcd8508076d7
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-08-26 17:06:25 -0300

    Update renderer.js

--- JavaScript/renderer.js
+++ JavaScript/renderer.js
@@ -15,7 +15,7 @@ function start()
 		document.getElementById("test").innerHTML="successfully retrieved web gl:\n\tLoading GL shader program \"basic\"...";
 		var program=createShaderProgram(gl,basicVertex,basicFragment);
 		let inLog=document.getElementById("test").innerHTML+":\n";
-		document.getElementById("test").innerHTML=inLog+"successfully loaded basic shader";
+		document.getElementById("test").innerHTML="successfully loaded basic shader";
 		aPos=gl.getAttribLocation(program"a_pos");
 		posBuff=gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER,posBuff);
@@ -25,6 +25,19 @@ function start()
 			0.0,0.5,
 			0.7,0.0
 		];
-		gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(positions),gl.STATIC_DRAW
+		gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(positions),gl.STATIC_DRAW);
+		resizeCanvasToDisplaySize(gl.canvas);
+		gl.viewport(0,0,gl.canvas.width,gl.canvas.height);
+		gl.clearColor(0,0,0,1);
+		gl.clear(gl.COLOR_BUFFER_BIT);
+		gl.useProgram(program);
+		gl.enableVertexAttribArray(aPos)
+		gl.bindBuffer(gl.ARRAY_BUFFER,posBuff);
+		
+		
+		
+		
+		
+		
 	}
 }
