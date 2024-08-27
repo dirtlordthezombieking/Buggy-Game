596275461af11d3ba8338337294ec7b689427b6e
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-08-27 14:07:07 -0300

    Update gl-tools.js

--- JavaScript/gl-tools.js
+++ JavaScript/gl-tools.js
@@ -28,46 +28,43 @@ function Uniform(uniformSize,shaderProgram,location,uniformValue)
 		}
 	};
 }
-function texture(shaderProgram,location,imageSrc)
+function texture(shaderProgram,location,imageSrc, referenceID)
 {
 	this.prog=shaderProgram;
 	this.image=imageSrc;
 	this.loc=gl.getUniformLocation(program,location);
-	this.use=function(pos)
+	this.id=referenceID;
+	this.texture;
+	this.push=function(pos)
 	{
-		let texture=gl.createTexture();
-		gl.bindTexture(gl.TEXTURE_2D,texture);
+		this.texture=gl.createTexture();
+		gl.bindTexture(gl.TEXTURE_2D,this.texture);
 		gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE);
 		gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE);
 		gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER,gl.NEAREST);
 		gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MAG_FILTER,gl.NEAREST);
 		gl.texImage2D(gl.TEXTURE_2D,0,gl.RGBA,gl.RGBA,gl.UNSIGNED_BYTE,this.image);
-		textures.push(texture);
-		
+		textures.push(this.texture);
+	}
+	this.use=function()
+	{
+		gl.uniform1i(u_image,id);
+		gl.activeTexture(gl.TEXTURE0+id);
+		gl.bindTexture(gl.TEXTURE_2D,this.texture);
 	}
 }
 function Attribute(attributeSize,shaderProgram,location, attributeValue)
 {
 	this.size=attributeSize;
 	this.prog=shaderProgram;
-	this.loc=gl.getUniformLocation(program,location);
+	this.loc=gl.getAtribLocation(program,location);
 	this.value=attributeValue;
 	this.use=function();
 	{
-		let texCoordBuff=gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER,texCoordBuff);
-		gl.bufferData(gl.ARRAY_BUFFER,new Float32Array
-		(
-			[
-				0.0,0.0,
-				1.0,0.0,
-				0.0,1.0,
-				0.0,1.0,
-				1.0,0.0,
-				1.0,1.0
-			]
-		),gl.STATIC_DRAW);
-		gl.enableVertexAttribArray(aTexCoord);
-		gl.vertexAttribPointer(aTexCoord,2,gl.FLOAT,false,0,0);
+		this.buff=gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER,buff);
+		gl.bufferData(gl.ARRAY_BUFFER,this.value,gl.STATIC_DRAW);
+		gl.enableVertexAttribArray(this.loc);
+		gl.vertexAttribPointer(this.loc,this.size,gl.FLOAT,false,0,0);
 	};
 }
