24478e0fb009192f5fb9af3487dd9b2f0bd1aad3
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-08-27 13:12:18 -0300

    Update gl-tools.js

--- JavaScript/gl-tools.js
+++ JavaScript/gl-tools.js
@@ -1,15 +1,22 @@
-function Uniform(uniformSize,glContext,shaderProgram,location,value)
+function Uniform(uniformSize,glContext,shaderProgram,location, uniformValue)
 {
 	this.size=uniformSize;
 	this.gl=glContext;
 	this.prog=shaderProgram;
 	this.loc=gl.getUniformLocation(program,location);
-	this.set=function(value);
+	this.value=uniformValue;
+	this.set=function(newValue);
 	{
-		
+		this.value=newValue;
 	}
 	this.use=function();
 	{
-		
+		if(this.size==1)
+		{
+			
+		}
+		if(this.size==1)
+		if(this.size==1)
+		if(this.size==1)
 	}
 }
