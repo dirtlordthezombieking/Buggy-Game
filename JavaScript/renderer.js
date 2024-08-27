b7252765358b4e8d9030fd87a6b660b7b71479f4
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-08-26 21:24:04 -0300

    Update renderer.js

--- JavaScript/renderer.js
+++ JavaScript/renderer.js
@@ -16,35 +16,4 @@ function start()
 	{
 		return;
 	}
-	aPos=gl.getAttribLocation(program,"a_pos");
-	uRes=gl.getUniformLocation(program,"u_res");
-	posBuff=gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER,posBuff);
-	let positions=
-	[
-		10.0,20.0,
-		80.0,20.0,
-		10.0,30.0,
-		10.0,30.0,
-		80.0,20.0,
-		80.0,30.0
-	];
-	gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(positions),gl.STATIC_DRAW);
-	resizeCanvasToDisplaySize(gl.canvas);
-	gl.viewport(0,0,gl.canvas.width,gl.canvas.height);
-	gl.clearColor(0,0,0,1);
-	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.useProgram(program);
-	gl.enableVertexAttribArray(aPos);
-	gl.bindBuffer(gl.ARRAY_BUFFER,posBuff);
-	let size=2;
-	let type=gl.FLOAT;
-	let normalize=false;
-	let stride=0;
-	let offset=0;
-	gl.vertexAttribPointer(aPos,size,type,normalize,stride,offset);
-	gl.uniform2f(uRes,gl.canvas.width,gl.canvas.height);
-	let primitiveType=gl.TRIANGLES;
-	let count=6;
-	gl.drawArrays(primitiveType,offset,count);
 }
