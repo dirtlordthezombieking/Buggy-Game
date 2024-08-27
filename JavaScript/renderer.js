b5fba18ed5e2be7e20d2adeeaec77f6e154720a0
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-08-26 21:33:03 -0300

    Update renderer.js

--- JavaScript/renderer.js
+++ JavaScript/renderer.js
@@ -16,4 +16,33 @@ function start()
 	{
 		return;
 	}
+	let uColour = gl.getUniformLocation(program,"u_colour");
+	for(var i=0;i<50;i++)
+	{
+		setRectangle(gl,randomInt(300),randomInt(300),randomInt(300),randomInt(300));
+	gl.uniform4f(colorUniformLocation,Math.random(),Math.random(),Math.random(),1);
+		gl.drawArrays(gl.TRIANGLES,0,6);
+	}
+}
+function randomInt(range)
+{
+	return Math.floor(Math.random()*range);
+}
+function setRectangle(gl,x,y,width,height)
+{
+	var x1=x;
+	var x2=x+width;
+	var y1=y;
+	var y2=y+height;
+	gl.bufferData(gl.ARRAY_BUFFER,new Float32Array
+	(
+		[
+			x1,y1,
+			x2,y1,
+			x1,y2,
+			x1,y2,
+			x2,y1,
+			x2,y2
+		]
+	),gl.STATIC_DRAW);
 }
