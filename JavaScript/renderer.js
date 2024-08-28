b4025b39e69468cfc16613dc40e31a3615c59722
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-08-28 14:12:56 -0300

    Update renderer.js

--- JavaScript/renderer.js
+++ JavaScript/renderer.js
@@ -3,12 +3,10 @@ let aPos;
 let aTexCoord;
 let uColour;
 let uRes;
-//let posBuff;
 let program;
 let image;
 function start()
 {
-	logMessage("start started");
 	let canvas = document.querySelector("#canvas");
 	gl = canvas.getContext("webgl");
 	if(!gl)
@@ -22,24 +20,15 @@ function start()
 		return;
 	}
 	resizeCanvasToDisplaySize(gl.canvas);
-	logMessage("image load started");
 	loadImage("images/stand in.png",function(img)
 	{
 		image=img;
-		logMessage("image loaded");
 		draw();
 	});
 }
 function draw()
 {
-	logMessage("draw started");
-	//aPos=gl.getAttribLocation(program,"a_pos");
-	//aTexCoord=gl.getAttribLocation(program,"a_texCoord");
-	logMessage("values set");
 //texcord
-	//let texCoordBuff=gl.createBuffer();
-	//gl.bindBuffer(gl.ARRAY_BUFFER,texCoordBuff);
-	//gl.bufferData(gl.ARRAY_BUFFER,
 	let aTexCoord=new Attribute(2,program,"a_texCoord",new Float32Array
 	(
 		[
@@ -52,29 +41,15 @@ function draw()
 		]
 	));
 	aTexCoord.use();
-//,gl.STATIC_DRAW);
-	//gl.enableVertexAttribArray(aTexCoord);
-	//gl.vertexAttribPointer(aTexCoord,2,gl.FLOAT,false,0,0);
 //texture
 	let text=new Texture(program,"u_colourTexture",image,0);
 	text.push();
 	text.use();
-	logMessage("image applied");
 //position
-	//posBuff=gl.createBuffer();
-	//gl.bindBuffer(gl.ARRAY_BUFFER,posBuff);
 	gl.viewport(0,0,gl.canvas.width,gl.canvas.height);
 	gl.clearColor(0,0,0,1);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 	gl.useProgram(program);
-	//gl.enableVertexAttribArray(aPos);
-	//gl.bindBuffer(gl.ARRAY_BUFFER,posBuff);
-	//let size=2;
-	//let type=gl.FLOAT;
-	//let normalize=false;
-	//let stride=0;
-	//let offset=0;
-	//gl.vertexAttribPointer(aPos,size,type,normalize,stride,offset);
 	uRes=new Uniform(2,program,"u_res",[gl.canvas.width,gl.canvas.height])
 	uRes.use()
 		let aPos=new Attribute(2,program,"a_pos",setRectangle(gl,randomInt(300),randomInt(300),randomInt(300),randomInt(300)));
@@ -93,7 +68,6 @@ function setRectangle(gl,x,y,width,height)
 	let x2=x+width;
 	let y1=y;
 	let y2=y+height;
-	//gl.bufferData(gl.ARRAY_BUFFER,
 	return new Float32Array
 	(
 		[
@@ -104,6 +78,6 @@ function setRectangle(gl,x,y,width,height)
 			x2,y1,
 			x2,y2
 		]
-	);//,gl.STATIC_DRAW);
+	);
 }
 logMessage("Renderer Version: 0.0.3 (8)");
