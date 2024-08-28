64e2b1e556a7e81dd33eef0647f680527e7951d8
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-08-28 10:39:31 -0300

    Update utils.js

--- JavaScript/utils.js
+++ JavaScript/utils.js
@@ -98,7 +98,7 @@ async function getTextData(src)
 			throw new Error("Error: "+response.status);
 		}
 		const text = await response.text();
-		logMessage("loaded: "text);
+		logMessage("loaded: "+text);
 	}
 	catch (e)
 	{
