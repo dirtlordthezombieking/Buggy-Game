27fd2cd49015f7734c66895d45d390144e122b1c
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-01 16:41:28 -0300

    Update cameras.js

--- JavaScript/cameras.js
+++ JavaScript/cameras.js
@@ -2,6 +2,7 @@ function Camera2D(width,height)
 {
 	this.w=width;
 	this.h=height;
+	this.loc=0;
 	this.transform=
 	[
 		 2 / width, 0,          0,
@@ -14,7 +15,7 @@ function Camera2D(width,height)
 	};
 	this.use=function(gl)
 	{
-		gl.uniformMatrix3fv(this.loc,false,this.total);
+		gl.uniformMatrix3fv(this.loc,false,this.transform);
 	};
 }
 logMessage("cameras Version: 0.0.0 (0)");
