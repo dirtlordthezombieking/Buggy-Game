d687dcd8c39616b9a02af996e06a012511cbed0b
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-01 16:31:18 -0300

    Update basic.js

--- shaders/basic.js
+++ shaders/basic.js
@@ -18,7 +18,7 @@ uniform mat3 u_transform;
 void main()
 {
   v_texCoord=a_texCoord;
-  vec2 pos=(u_proj,qvec3((u_transform*vec3(a_pos.xy,1)).xy,1)).xy;
+  vec2 pos=(u_proj,vec3((u_transform*vec3(a_pos.xy,1)).xy,1)).xy;
   gl_Position=vec4(pos,0,1);
 }`
 logMessage("Basic Shader Version: 0.0.5 (0)");
