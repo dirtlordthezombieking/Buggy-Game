be020672aeb91c416443c9cfceead481111558df
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-01 20:39:47 -0300

    Update utils.js

--- JavaScript/utils.js
+++ JavaScript/utils.js
@@ -81,11 +81,12 @@ async function getTextData(src,onDone)
 function glSetup(gl)
 {
 	gl.enable(gl.CULL_FACE);
+	gl.enable(gl.DEPTH_TEST);
 	gl.viewport(0,0,gl.canvas.width,gl.canvas.height);
 }
 function glStartFrame(gl,r,g,b,a)
 {
 	gl.clearColor(r,g,b,a);
-	gl.clear(gl.COLOR_BUFFER_BIT);
+	gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
 }
 logMessage("Utils Version: 0.0.11 (0)");
