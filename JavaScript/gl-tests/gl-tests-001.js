eff90f52fac6a30f0209ed24868224b6ac197d7f
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-02 10:16:02 -0300

    Update gl-tests-001.js

--- JavaScript/gl-tests/gl-tests-001.js
+++ JavaScript/gl-tests/gl-tests-001.js
@@ -44,7 +44,7 @@ function GLTest001(shaderProgram,glCore)
 		//this.uRes.use();
 		this.uProj=new Camera2D(this.gl.canvas.width,this.gl.canvas.height);
 		this.uProj.setData(this.program,"u_proj",this.gl);
-		//this.uProj.transform.setRotation(20.0);
+		this.uProj.transform.setRotation(20.0);
 		//this.uProj.transform.setTranslation(0.0,100.0)
 		this.uProj.update()
 		this.uProj.use(this.gl);
