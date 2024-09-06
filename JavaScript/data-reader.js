9ac9ba8c1c5944626bccd7851f6dbabc894a25b0
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-06 13:40:49 -0300

    Update data-reader.js

--- JavaScript/data-reader.js
+++ JavaScript/data-reader.js
@@ -1,35 +1,95 @@
-function DataReader
+function DataReader()
 {
 	this.readData=async function(src)
 	{
 		await getTextData(src,function(str)
 		{
-			let read=new StringProcesser(str);
-			let line=read.nextLine();
-			if(line===":::{}:::")
+			try
 			{
+				let read=new StringProcesser(str);
+				let line=read.nextLine();
+				if(line===":::{}:::")
+				{
+				}
+				else if(line===":::[]:::")
+				{
+					log message(e.
+				}
+				else
+				{
+					throw new error("Invalid data entry type: \'"+line+"\'.");
+				}
 			}
-			else if(line===":::[]:::")
+			catch(e)
 			{
+
 			}
-			else
-			{
-				throw new error("Invalid data entry type: \'"+line+"\'.");
-			}
-		}));
+		});
 	}
 	this.parseElement=function(reader)
 	{
 		let line=reader.nextLine().split":";
+		if(line[0]==="S")
+		{
+			return line[1];
+		}
+		if(line[0]==="I")
+		{
+			return parseInt(line[1]);
+		}
+		if(line[0]==="F")
+		{
+			return parseFloat(line[1]);
+		}
+		if(line[0]===">I")
+		{
+			return parseIntArray(line[1]);
+		}
+		if(line[0]===">F")
+		{
+			return parseFloatArray(line[1]);
+		}
+		if(line[0]==="[]")
+		{
+			//return line[1];
+		}
+		if(line[0]==="{}")
+		{
+			//return line[1];
+		}
+		throw new error("Invalid data type: \'"+
+	}
+	this.parseIntArray=function(src)
+	{
+		let out=src.replace(" ","").replace("\n","").split(",");
+		for(let i=0;i<out.length;i++)
+		{
+			out[i]=parseInt(out[i]);
+		}
+		return out;
+	}
+	this.parseFloatArray=function(src)
+	{
+		let out=src.replace(" ","").replace("\n","").split(",");
+		for(let i=0;i<out.length;i++)
+		{
+			out[i]=parseFloat(out[i]);
+		}
+		return out;
 	}
 }
 function StringProcesser(txt)
 {
 	this.str=txt;
 	this.pos=0;
+	this.line=0;
 	this.next=function()
 	{
 		let c=str[pos];
+		if(c==="\n")
+		{
+			line++;
+		}
 		this.pos=this.pos+1;
 		return c;
 	};
