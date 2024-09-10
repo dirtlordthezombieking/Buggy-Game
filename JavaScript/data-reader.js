acd993080bb9ad4e19804328a328ec46d54a2999
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-09 21:52:42 -0300

    Update data-reader.js

--- JavaScript/data-reader.js
+++ JavaScript/data-reader.js
@@ -1,1 +1,59 @@
 logMessage("Data Reader start");
+
+logMessage("Data Reader string processer");
+function StringProcesser(txt)
+{
+	this.str=txt;
+	this.pos=0;
+	this.line=0;
+	this.next=function()
+	{
+		let c=this.str[this.pos];
+		if(c==="\n")
+		{
+			this.line++;
+		}
+		this.pos=this.pos+1;
+		return c;
+	};
+	this.nextLine=function()
+	{
+		let char=this.next();
+		let line="";
+		while(!(char==="\n"))
+		{
+			line=line+char;
+			char=this.next();
+		}
+		return line;
+	};
+	this.until=function(end)
+	{
+		let char=this.next();
+		let line="";
+		while(!(char===end))
+		{
+			line=line+char;
+			char=this.next();
+		}
+		return line;
+	};
+	this.getDepth=function()
+	{
+		let i=0;
+		let char=this.next();
+		while(!(char==="\t"))
+		{
+			char=this.next();
+			i=i+1;
+		}
+		this.pos=this.pos-1;
+		return i;
+	};
+}
+logMessage("Data Reader passable reference");
+function PassableReference(item)
+{
+	this.value=item;
+}
+logMessage("Data Reader Version: 0.0.0");
