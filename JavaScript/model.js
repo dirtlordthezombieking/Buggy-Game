d84eb09476edc475311a86c2fb4f06a7c7af0ab8
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-09-20 17:53:17 -0300

    Update model.js

--- JavaScript/model.js
+++ JavaScript/model.js
@@ -3,6 +3,10 @@ function Model(glCore,shaderProgram,src)
 	this.gl=glCore;
 	this program=shaderProgram;
 	let meshes=src["MESH"];
+	meshes.forEach(function(mesh,ind)
+	{
+		
+	});
 }
 function RawAttribute()
 {
